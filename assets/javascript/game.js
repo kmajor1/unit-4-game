d2b0a237f7f2dde837800875338f8ed5ed1b9aed
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-10-13 18:08:03 -0400

    introduced attack function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,47 +2,61 @@ $(document).ready(function () {
     // main code here 
     // global variables 
     var rpgGame = {
-        endState: true,
+        isGameStarted: false,
         playerCharacter: {
             codeName: "",
             displayName: "", 
             healthPts: 0, 
-            attackPts: 0, 
+            attackPts: {
+                base: 0, 
+                current: 0
+            },
             counterAttackPts: 0, 
-            isDefeated: false 
+            isDefeated: false, 
+            attack: function () {
+                
+                // reduce the opponent character object's health value 
+                // invoke a counter attack on the playerCharacter (reduce playerChar health)
+                // invoke a "check" status function 
+                // to check health of opponent and character post-interaction
+                // take the current attack value, increase by base value
+                rpgGame.playerCharacter.attackPts.current += rpgGame.playerCharacter.attackPts.base;
+
+
+            }
         },
         opponentCharacter: "", 
         characters: [
             {
                 codeName: "darthVader",
                 displayName: "Darth Vader", 
-                healthPts: 500,
-                attackPts: 200,
-                counterAttackPts: 200,
+                healthPts: 125,
+                attackPts: 25,
+                counterAttackPts: 25,
                 isDefeated: false 
             },
             {
                 codeName: "obiWan",
                 displayName: "Obi-Wan Kenobi", 
-                healthPts: 500,
-                attackPts: 200,
-                counterAttackPts: 200,
+                healthPts: 200,
+                attackPts: 25,
+                counterAttackPts: 35,
                 isDefeated: false
             },
             {
                 codeName: "rey",
                 displayName: "Rey",
-                healthPts: 500,
-                attackPts: 200, 
-                counterAttackPts: 200, 
+                healthPts: 100,
+                attackPts: 35, 
+                counterAttackPts: 30, 
                 isDefeated: false 
             },
             {
                 codeName: "han",
                 displayName: "Han Solo",
-                healthPts: 500, 
-                attackPts: 200, 
-                counterAttackPts: 200,  
+                healthPts: 130, 
+                attackPts: 15, 
+                counterAttackPts: 25,  
                 isDefeated: false 
             }
         ],
@@ -65,10 +79,10 @@ $(document).ready(function () {
             this.playerCharacter.codeName = this.characters[i].codeName;
             this.playerCharacter.displayName = this.characters[i].displayName;
             this.playerCharacter.healthPts = this.characters[i].healthPts;
-            this.playerCharacter.attackPts = this.characters[i].attackPts;
+            this.playerCharacter.attackPts.base = this.characters[i].attackPts;
             this.playerCharacter.counterAttackPts = this.characters[i].counterAttackPts;
-            console.log("Player Character Object");
-            console.log(this.playerCharacter);
+            
+            
 
             
             
@@ -77,10 +91,10 @@ $(document).ready(function () {
             this.opponentCharacter = charCode; 
         },
         startGame: function () {
-            this.endState = false; 
+            this.isGameStarted = true; 
         },
-        attack: function () {
-
+        endGame: function () {
+            this.isGameStarted = false; 
         }
     }
     // events 
@@ -89,4 +103,8 @@ $(document).ready(function () {
     rpgGame.selectCharacter("darthVader");
     console.log("Game Object");
     console.log(rpgGame);
+    rpgGame.playerCharacter.attack();
+    rpgGame.playerCharacter.attack();
+    console.log("Player Character Object");
+    console.log(rpgGame.playerCharacter);
 })
