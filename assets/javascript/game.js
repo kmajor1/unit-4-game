758647c98142666baaeede9066a34ba34b0e12cb
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-10-13 13:11:19 -0400

    added rpgGame object. added character object nested within game object.

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+$(window).on("load", function () {
+    // main code here 
+    // global variables 
+    var rpgGame = {
+        endstate: false,
+        characters: [
+            {
+                codeName: "darthVader",
+                displayName: "Darth Vader", 
+                healthPts: 500,
+                attackPts: 200,
+                counterAttackPts: 200,
+                isNPC: false, 
+                isDefeated: false 
+
+            },
+            {
+                codeName: "darthVader",
+                displayName: "Darth Vader", 
+                healthPts: 500,
+                attackPts: 200,
+                counterAttackPts: 200,
+                isNPC: false,
+                isDefeated: false
+            } 
+        ]
+    }
+    // events 
+        // click any character 
+        //
+})
