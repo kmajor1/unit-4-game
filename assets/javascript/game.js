c18aec5d922725a1220037d0d55553afec928b6d
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-10-15 13:23:16 -0400

    added image assets. improved selectChar and selectOpp functions.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,17 +24,26 @@ $(document).ready(function () {
                     // set the opponent health to 0
                     rpgGame.opponentCharacter.healthPts.current = 0;
                     //invoke characterDefeated 
+                    this.charDefeated("e"); 
                 }
-                // reduce the opponent character object's health value
-                rpgGame.opponentCharacter.healthPts -= rpgGame.playerCharacter.attackPts.current;
-                // invoke a counter attack on the playerCharacter (reduce playerChar health)
-                rpgGame.playerCharacter.healthPts.current -= rpgGame.opponentCharacter.counterAttackPts;
-                // invoke a "check status" function 
-                // to check health of opponent and character post-interaction
-                // take the current attack value, increase by base value
-                rpgGame.playerCharacter.attackPts.current += rpgGame.playerCharacter.attackPts.base;
-
-
+                else {
+                    // reduce the opponent character object's health value
+                    rpgGame.opponentCharacter.healthPts -= rpgGame.playerCharacter.attackPts.current;
+                    // invoke a counter attack on the playerCharacter (reduce playerChar health)
+                    // check whether this will defeat the player 
+                    if (rpgGame.opponentCharacter.counterAttackPts >= rpgGame.playerCharacter.healthPts.current) {
+                        // set player character health to 0 
+                        rpgGame.playerCharacter.healthPts.current = 0;
+                        // invoke charDefeated routine 
+                        this.charDefeated("p"); 
+                    }
+                    else {
+                        rpgGame.playerCharacter.healthPts.current -= rpgGame.opponentCharacter.counterAttackPts;
+                        // take the current attack value, increase by base value
+                        rpgGame.playerCharacter.attackPts.current += rpgGame.playerCharacter.attackPts.base;
+                    }
+                    
+                }
             }
         },
         opponentCharacter: {
@@ -99,23 +108,71 @@ $(document).ready(function () {
             }
             this.playerCharacter.codeName = this.characters[i].codeName;
             this.playerCharacter.displayName = this.characters[i].displayName;
-            this.playerCharacter.healthPts = this.characters[i].healthPts;
+            this.playerCharacter.healthPts.base = this.characters[i].healthPts;
+            this.playerCharacter.healthPts.current = this.playerCharacter.healthPts; 
             this.playerCharacter.attackPts.base = this.characters[i].attackPts;
             this.playerCharacter.counterAttackPts = this.characters[i].counterAttackPts;
         },
         selectOpponent: function (charCode) {
-            this.opponentCharacter = charCode; 
-        },
-        charDefeated: function () {
-            // check if the character defeated is the last in contention 
-            if (this.opponentsRem > 1) {
-                // remove the opponent from the defender area 
-                // place game in a state where the user can select another opponent 
-                // invoke selectOpponent method 
+            // set indexes for the different characters 
+            if (charCode == "darthVader") {
+                i = 0; 
             }
+            else if (charCode == "obiWan") {
+                i = 1; 
+            }
+            else if (charCode == "rey") {
+                i = 2; 
+            }
+            else if (charCode == "han") {
+                i = 3; 
+            } 
+            // set the opponent character properties to selected opponent 
+            this.opponentCharacter.codeName = this.characters[i].codeName; 
+            this.opponentCharacter.displayName = this.characters[i].displayName;
+            this.opponentCharacter.healthPts.base = this.characters[i].healthPts
+            this.opponentCharacter.healthPts.current = this.characters[i].healthPts;
+            this.opponentCharacter.counterAttackPts = this.characters[i].counterAttackPts;
+
+            // move the selected opponent into the defender area 
+
         },
-        startGame: function () {
+        charDefeated: function (defeatCode) {
+            // check if the char being evaluated is a player or opponent character
+            // defeatCode: string: p --> player, anything else, enemy
+            if (defeatCode == "p") {
+                // set isCharacterDefeated to true 
+                this.isGameStarted = false; 
+                // give user a reset button 
+                
+            }
+            else {
+                // check if there's more opponents to be played 
+                if (this.opponentsRem > 1) {
+                    // remove the opponent from the defender area 
+                    // place game in a state where the user can select another opponent 
+                    this.isOpponentSelected = false; 
+                    // invoke a message to the user that they've defeated the opponent 
+                    // remove the opponent from the screen 
+                    
+                }
+                else {
+                    // set isGameStarted to false 
+                    this.isGameStarted = false; 
+                    // invoke a message to the user that they've beaten the specific opponent
+                    // invoke a message that the user that they've beaten the game 
+                    // invoke a reset button for the user 
+                }
+            }
+            // check if the character defeated is the last in contention 
+            
+        },
+        updateScreenArea: function () {
+
+        },
+        startGame: function () { // this is the "reset" function 
             this.isGameStarted = true; 
+            // clear all appropriate screen areas 
         },
         endGame: function () {
             this.isGameStarted = false; 
