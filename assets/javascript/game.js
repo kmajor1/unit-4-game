57d08203c72075652d538af600669bb5ffe8c5e0
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-10-15 22:49:37 -0400

    f added onclick functions for player and opponent selection

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
                     // set the opponent health to 0
                     rpgGame.opponentCharacter.healthPts.current = 0;
                     //invoke characterDefeated 
-                    this.charDefeated("e"); 
+                    rpgGame.charDefeated("e"); 
                 }
                 else {
                     // reduce the opponent character object's health value
@@ -35,7 +35,7 @@ $(document).ready(function () {
                         // set player character health to 0 
                         rpgGame.playerCharacter.healthPts.current = 0;
                         // invoke charDefeated routine 
-                        this.charDefeated("p"); 
+                        rpgGame.charDefeated("p"); 
                     }
                     else {
                         rpgGame.playerCharacter.healthPts.current -= rpgGame.opponentCharacter.counterAttackPts;
@@ -96,15 +96,19 @@ $(document).ready(function () {
             // determine the player selected, set index value 
             if (charCode == "darthVader") {
                 i = 0; 
+                darthVaderBox.appendTo("#yourCharacterSpace");
             }
             else if (charCode == "obiWan") {
                 i = 1; 
+                obiWanBox.appendTo("#yourCharacterSpace");
             }
             else if (charCode == "rey") {
                 i = 2; 
+                reyBox.appendTo("#yourCharacterSpace");
             }
             else if (charCode == "han") {
                 i = 3; 
+                hanSoloBox.appendTo("#yourCharacterSpace")
             }
             this.playerCharacter.codeName = this.characters[i].codeName;
             this.playerCharacter.displayName = this.characters[i].displayName;
@@ -112,20 +116,32 @@ $(document).ready(function () {
             this.playerCharacter.healthPts.current = this.playerCharacter.healthPts; 
             this.playerCharacter.attackPts.base = this.characters[i].attackPts;
             this.playerCharacter.counterAttackPts = this.characters[i].counterAttackPts;
+
+            console.log(this.playerCharacter);
+
+            // set game object state to player character selected 
+            this.isPlayerCharacterSelected = true; 
+
+            // can I start a round/game? 
+            this.startGame()
         },
         selectOpponent: function (charCode) {
             // set indexes for the different characters 
             if (charCode == "darthVader") {
                 i = 0; 
+                darthVaderBox.appendTo("#currentOpponentBox");
             }
             else if (charCode == "obiWan") {
                 i = 1; 
+                obiWanBox.appendTo("#currentOpponentBox");
             }
             else if (charCode == "rey") {
                 i = 2; 
+                reyBox.appendTo("#currentOpponentBox");
             }
             else if (charCode == "han") {
                 i = 3; 
+                hanSoloBox.appendTo("#currentOpponentBox");
             } 
             // set the opponent character properties to selected opponent 
             this.opponentCharacter.codeName = this.characters[i].codeName; 
@@ -133,8 +149,10 @@ $(document).ready(function () {
             this.opponentCharacter.healthPts.base = this.characters[i].healthPts
             this.opponentCharacter.healthPts.current = this.characters[i].healthPts;
             this.opponentCharacter.counterAttackPts = this.characters[i].counterAttackPts;
-
-            // move the selected opponent into the defender area 
+            console.log(this.opponentCharacter);
+            
+            // set game object state to player character selected 
+            this.isOpponentSelected = true; 
 
         },
         charDefeated: function (defeatCode) {
@@ -170,22 +188,128 @@ $(document).ready(function () {
         updateScreenArea: function () {
 
         },
-        startGame: function () { // this is the "reset" function 
-            this.isGameStarted = true; 
+        startGame: function () { //  
+            // are both player and character selected? 
+            if (rpgGame.isPlayerCharacterSelected && rpgGame.isOpponentSelected) {
+                this.isGameStarted = true; 
+            }
+            
             // clear all appropriate screen areas 
         },
-        endGame: function () {
+        resetGame: function () { // reset function??? 
             this.isGameStarted = false; 
         }
     }
-    // events 
-        // click any character 
-        //
-    rpgGame.selectCharacter("darthVader");
+
+    // debug stuff
     console.log("Game Object");
     console.log(rpgGame);
-    rpgGame.playerCharacter.attack();
-    rpgGame.playerCharacter.attack();
-    console.log("Player Character Object");
-    console.log(rpgGame.playerCharacter);
+    
+
+    // load all character boxes into their own containers 
+    var darthVaderBox = $("#darthVader");
+    var obiWanBox = $("#obiwan");
+    var reyBox = $("#rey");
+    var hanSoloBox = $("#han");
+    
+    // log those containers 
+    
+
+    // assign click events to each 
+    // darthVader on click 
+    darthVaderBox.on("click", function (event) {
+        // if the game has started, clicking a character does nothing 
+        if (rpgGame.isGameStarted == true) {
+            // does nothing, maybe show something on screen? 
+        }
+        else {
+            // check if a playerCharacter and opponent are NOT selected 
+            
+            if ((rpgGame.isPlayerCharacterSelected == false) && (rpgGame.isOpponentSelected == false)) {
+                // invoke select player for darth 
+                rpgGame.selectCharacter("darthVader");
+            }
+            else if ((rpgGame.isPlayerCharacterSelected == true) && (rpgGame.isOpponentSelected == false)) {
+                // invoke select opponent 
+                rpgGame.selectOpponent("darthVader");
+                // set the game ready to start 
+                rpgGame.isGameStarted = true; 
+            }
+        }
+        
+        
+
+        
+    })
+
+    // obiWan onclick 
+    obiWanBox.on("click", function (event) {
+        alert("click");
+        // if the game has started, clicking a character does nothing 
+        if (rpgGame.isGameStarted == true) {
+            // do nothing 
+        }
+        else {
+            // check if a playerCharacter and opponent are NOT selected 
+            
+            if ((rpgGame.isPlayerCharacterSelected == false) && (rpgGame.isOpponentSelected == false)) {
+                // invoke select player for darth 
+                rpgGame.selectCharacter("obiWan");
+            }
+            else if ((rpgGame.isPlayerCharacterSelected == true) && (rpgGame.isOpponentSelected == false)) {
+                // invoke select opponent 
+                rpgGame.selectOpponent("obiWan");
+                // set the game ready to start 
+                rpgGame.isGameStarted = true; 
+            }
+        }
+    })
+
+    // rey onclick 
+    reyBox.on("click", function (event) {
+        alert("click");
+        // if the game has started, clicking a character does nothing 
+        if (rpgGame.isGameStarted == true) {
+            // do nothing 
+        }
+        else {
+            // check if a playerCharacter and opponent are NOT selected 
+            
+            if ((rpgGame.isPlayerCharacterSelected == false) && (rpgGame.isOpponentSelected == false)) {
+                // invoke select player for darth 
+                rpgGame.selectCharacter("rey");
+            }
+            else if ((rpgGame.isPlayerCharacterSelected == true) && (rpgGame.isOpponentSelected == false)) {
+                // invoke select opponent 
+                rpgGame.selectOpponent("rey");
+                // set the game ready to start 
+                rpgGame.isGameStarted = true; 
+            }
+        }
+    })
+
+    // han onclick 
+
+    hanSoloBox.on("click", function () {
+        alert("click");
+        // if the game has started, clicking a character does nothing 
+        if (rpgGame.isGameStarted == true) {
+            // do nothing 
+        }
+        else {
+            // check if a playerCharacter and opponent are NOT selected 
+            
+            if ((rpgGame.isPlayerCharacterSelected == false) && (rpgGame.isOpponentSelected == false)) {
+                // invoke select player for darth 
+                rpgGame.selectCharacter("han");
+            }
+            else if ((rpgGame.isPlayerCharacterSelected == true) && (rpgGame.isOpponentSelected == false)) {
+                // invoke select opponent 
+                rpgGame.selectOpponent("han");
+                // set the game ready to start 
+                rpgGame.isGameStarted = true; 
+            }
+        }
+    })
+
 })
